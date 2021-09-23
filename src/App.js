23cab9516e113ff4ab111b76b0e7bf304438600b
saifulislammunna <saifulmunna28@gmail.com>
2021-09-23 10:45:50 +0600

    understanding react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Device from './components/Device/Device';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [steps, setSteps] = useState(0);
+  const handleIncreaseSteps = () => {
+    const newStepsCount = steps + 1;
+    setSteps(newStepsCount);
+     
+
+  } 
+  useEffect( ()=>{
+    console.log(steps);
+  }, [steps])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>My Steps: {steps}</h3>
+      <button onClick={handleIncreaseSteps}>Walk</button>
+     <Device name="phone" steps={ steps} price="13500"/> 
+       
     </div>
   );
 }
